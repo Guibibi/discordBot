3796b645ddb321fa9fac23eb3be6ba66769e463c
Guibibi <bgui34@gmail.com>
2020-10-23 17:51:53 -0400

    Removed extra code

--- commands/joinVoice.js
+++ commands/joinVoice.js
@@ -7,25 +7,10 @@ module.exports = {
 		if (!message.guild) return console.log('Need to be in a server.');
 		let connection = null;
 
-		/* 		if (message.member.voice.channel) {
+		if (message.member.voice.channel) {
 			connection = message.member.voice.channel.join();
 		} else {
 			message.reply('You need to join a voice channel first!');
 		}
-		
-		const dispatcher = connection.play('/audio/caliss.mp3');
-		 */
-
-		dispatcher.on('start', () => {
-			console.log('caliss.mp3 is now playing!');
-		});
-
-		dispatcher.once('finish', () => {
-			console.log('Finished playing!');
-		});
-
-		dispatcher.on('error', console.error);
-
-		dispatcher.destroy();
 	}
 };
